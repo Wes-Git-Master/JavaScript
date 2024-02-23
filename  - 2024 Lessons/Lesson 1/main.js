35e25d03e07017583ce726aad9f9926e2db19d9a
Едвін <edvinfaas@gmail.com>
2024-02-23 21:58:12 +0200

    2024 - Lesson 2

---
+++  - 2024 Lessons/Lesson 1/main.js
@@ -0,0 +1,170 @@
+console.log('jasgjgsa');
+
+// declaration
+// var a;
+// // string bumber boolan
+// // init
+// a = "asdasd";
+// a = 153542.14123;
+// a = false;
+//
+// var b = 'ashgfdh';
+
+// let a = 'okten';
+// console.log(a); // okten
+// a = 10005000;
+// console.log(a); // 100500
+
+// let data = 'okten';
+// console.log(data);
+// console.log(data);
+// console.log(data);
+// console.log(data);
+
+
+// {
+//     var x = ' kagdshgsa';
+//
+//
+// }
+//
+//
+//
+//
+// {
+//     console.log(x);
+//
+// }
+
+
+// const c = 'ashgfdahgs';
+// c = 'gasfdhgsafd';
+
+// let w1 = 'okten';
+// let w2 = 'is';
+// let w3 = '\'sucks\'';
+// let space = ' ';
+//
+// let sen = w1 + space + w2 + space + w3;
+// console.log(sen);
+//
+// let sen2 = `${w1} ${w2} ${w3}!`;
+// console.log(sen2);
+
+// let n1 = 10;
+// let n2 = 20;
+// let res1 = n1 + n2;
+// console.log(n1 + n2);
+// console.log(res1);
+//
+// console.log(15 % 3);
+
+
+// let bool1 = true; //1
+// let bool2 = false;// 0
+// console.log(bool1 + bool2);
+// console.log(10 - true);
+
+
+// console.log(100 + 'asd'); //
+// console.log(100 + 200 + 'ads');
+// console.log('asd' + 100 + 200);
+// console.log('asd' - 1); //NaN
+// console.log('11' - 1); //10
+// console.log('11' - true); //10
+//
+// console.log('asdsa' - '123123');// NaN
+
+// NaN
+// console.log(typeof NaN);
+// console.log(typeof 123);
+// console.log(typeof '123');
+// console.log(typeof true);
+//
+// let x = 1232131;
+//
+// console.log(typeof x);
+
+
+// let xxx;
+// console.log(xxx);
+// null
+// console.log(typeof null);
+// let yyy = null;
+
+
+// let numToStr = 123213 + '';
+// console.log(typeof numToStr, numToStr);
+//
+// console.log((+'123') + 10);
+// console.log(10 + (+'123'));
+//
+// console.log(+'123d');
+// console.log(parseInt('123asd143'));
+
+// alert('asjhgasjhg');
+// let b = confirm('aksjgdjhgsa');
+// console.log(b);
+
+// let name = +prompt('enter you name');
+// console.log(name);
+
+
+// console.log(!!'ashgdsahgf');
+// console.log(!!' ');
+// console.log(!!'');// false
+// console.log(!!-1000);
+// console.log(!!0);// false
+// console.log(!!null); // false
+// console.log(!!undefined);  // false
+// console.log(!!NaN);  // false
+
+// > < >= <= == === != !==
+// console.log(5 > 6);
+// console.log(5 !== 6);
+
+// console.log('5' === 5);
+// '5' == 5 -> as314 == 5
+
+
+let obj1 = {
+    name: 'sergii',
+    age: 36
+}
+
+console.log(obj1);
+
+
+let obj2 = {
+    name: 'kokos',
+    age: 123,
+    wife: {
+        name: 'abrikoska',
+        age: 87
+    }
+}
+
+console.log(obj2.name);
+console.log(obj2);
+console.log(obj2.name);
+console.log(obj2.wife);
+console.log(obj2.wife.name);
+console.log(obj2['wife']['name']);
+console.log(obj2['wife'].name);
+
+//                                    0      1            2                3         4         5    6
+//                                   ____  ____   _______________________  ____   _________   __  ________
+let arr = [obj1, obj2, {name: 'vasya', age: 31}, true, 'ahgdasfhg', [], 123123123];
+console.log(arr);
+
+console.log(arr[3]);
+console.log(arr[2]);
+arr[2] = {name: 'petya', age: 765434324}
+console.log(arr);
+console.log(arr[6]);
+
+console.log(arr[2].name);
+console.log(arr[2].age);
+
+
+

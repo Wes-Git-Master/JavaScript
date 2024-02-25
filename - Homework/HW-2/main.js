0cd9fc2cf18f6c0b891022fc78b3d60f18aac46d
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-02-25 20:24:06 +0200

    Homework 2 done

--- - Homework/HW-2/main.js
+++ - Homework/HW-2/main.js
@@ -198,3 +198,41 @@ if (x2 === false || +false) {
 //      У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
 
 console.log(symbol);
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log("Супер")
+} else {
+    console.log('5 or less')
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log("Супер")
+} else {
+    console.log('5 or less')
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log("Супер")
+} else {
+    console.log('5 or less')
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log("Супер")
+} else {
+    console.log('5 or less')
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log("Супер")
+} else {
+    console.log('5 or less')
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log("Супер")
+} else {
+    console.log('5 or less')
+}

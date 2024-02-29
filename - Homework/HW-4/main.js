40a6c6ac81d928797d3dce2096f355602584f6bd
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-02-29 22:24:34 +0200

    Homework 4

--- - Homework/HW-4/main.js
+++ - Homework/HW-4/main.js
@@ -68,27 +68,79 @@ build2('Text-2')
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
 //   Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
-function build3(text, res) {
+function build3(text, number) {
     for (let i = 0; i < 1; i++) {
-        document.write(
-            `<ul></ul>`
-        )
-        for (let j = 0; j < res; j++) {
-            document.write(
-                `<li>${text} </li>`
-            )
+        document.write(`<ul>`)
+        for (let j = 0; j < number; j++) {
+            document.write(`<li>${text} </li>`)
         }
+        document.write(`</ul>`)
     }
-    return res
 }
 
 build3('Text-3', 3)
 
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+let arr2 = ['world', 10500, true, false];
+
+function build4() {
+    for (const argument of arguments) {
+        document.write(`<ul>`)
+        for (let i = 0; i < argument.length; i++) {
+            const argumentElement = argument[i];
+            document.write(`<ul>  ${argumentElement}</ul>`)
+        }
+        document.write(`</ul>`)
+    }
+}
+
+build4(arr2)
+
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.
 //   Для кожного об'єкту окремий блок.
+
+let users2 = [
+    {id: 1, name: 'vasya', age: 31},
+    {id: 2, name: 'petya', age: 30},
+    {id: 3, name: 'kolya', age: 29},
+    {id: 4, name: 'olya', age: 28},
+    {id: 5, name: 'max', age: 19},
+];
+
+
+function build5() {
+    for (const argument of arguments) {
+        for (let i = 0; i < argument.length; i++) {
+            const argumentElement = argument[i];
+            document.write(`<div>${argumentElement.id} ${argumentElement.name} ${argumentElement.age}</div> `)
+        }
+    }
+}
+
+build5(users2)
+
 // - створити функцію яка повертає найменьше число з масиву
+let arr3 = [100, -18, 3, 4, 5, 10, -200, 500, 600];
+
+function minNumber() {
+    let minNum = arguments[0]
+    let maxNum = arguments[0]
+    for (const argument of arguments) {
+        if (argument < minNum) {
+            minNum = argument
+        } else if (argument > maxNum) {
+            maxNum = argument
+        }
+    }
+    console.log('min', minNum)
+    return minNum
+}
+
+minNumber(100, -18, 3, 4, 5, 10, -200, 500, 600)
+
+
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]

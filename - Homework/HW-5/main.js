14e2fce14fada1f4bc3fbc346f30c441c62b5ad0
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-03-03 18:06:32 +0200

    Homework 5

--- - Homework/HW-5/main.js
+++ - Homework/HW-5/main.js
@@ -138,6 +138,7 @@ foo10(arrNumbers)
 
 
 
+
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)

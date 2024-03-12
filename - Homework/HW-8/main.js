adfbcf6f5cb1b694e444ef47898aa0e65f0945f4
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-03-12 19:27:43 +0200

    Homework 8

--- - Homework/HW-8/main.js
+++ - Homework/HW-8/main.js
@@ -47,23 +47,20 @@ class Client {
 
 let ClientArr = [
     new Client(1, 'Max', 'Ivanovich', 'Max@com.ua', 380111111111, ['qwe']),
-    new Client(2, 'Ivan', 'Maximovich', 'Ivan@com.ua', 380222222222,['qwe', 'asd', 'zxc']),
-    new Client(3, 'Yuriy', 'Kirilovich', 'Yuriy@com.ua', 380333333333,['qwe', 'asd']),
-    new Client(4, 'Kiril', 'Yurievich', 'Kiril@com.ua', 380444444444,['qwe', 'asd', 'zxc','qwe', 'asd', 'zxc']),
-    new Client(5, 'Anna', 'Vasylivna', 'Anna@com.ua', 380555555555,['qwe', 'asd', 'zxc','qwe']),
-    new Client(6, 'Olena', 'Stepanivna', 'Olena@com.ua', 380666666666,['qwe', 'asd', 'zxc']),
-    new Client(7, 'Iryna', 'Oleksandrivna', 'Iryna@com.ua', 380777777777,['qwe', 'asd', 'zxc','qwe', 'asd']),
-    new Client(8, 'Stepan', 'Matvyevich', 'Stepan@com.ua', 380888888888,['qwe', 'asd', 'zxc']),
-    new Client(9, 'Anastasia', 'Oleksandrivna', 'Nastya@com.ua', 380999999999,['qwe', 'asd', 'zxc','qwe', 'asd', 'zxc','qwe', 'asd', 'zxc']),
-    new Client(10, 'Robert', 'Pavlovich', 'Robert@com.ua', 380101010101,['qwe', 'asd', 'zxc']),
+    new Client(2, 'Ivan', 'Maximovich', 'Ivan@com.ua', 380222222222, ['qwe', 'asd', 'zxc']),
+    new Client(3, 'Yuriy', 'Kirilovich', 'Yuriy@com.ua', 380333333333, ['qwe', 'asd']),
+    new Client(4, 'Kiril', 'Yurievich', 'Kiril@com.ua', 380444444444, ['qwe', 'asd', 'zxc', 'qwe', 'asd', 'zxc']),
+    new Client(5, 'Anna', 'Vasylivna', 'Anna@com.ua', 380555555555, ['qwe', 'asd', 'zxc', 'qwe']),
+    new Client(6, 'Olena', 'Stepanivna', 'Olena@com.ua', 380666666666, ['qwe', 'asd', 'zxc']),
+    new Client(7, 'Iryna', 'Oleksandrivna', 'Iryna@com.ua', 380777777777, ['qwe', 'asd', 'zxc', 'qwe', 'asd']),
+    new Client(8, 'Stepan', 'Matvyevich', 'Stepan@com.ua', 380888888888, ['qwe', 'asd', 'zxc']),
+    new Client(9, 'Anastasia', 'Oleksandrivna', 'Nastya@com.ua', 380999999999, ['qwe', 'asd', 'zxc', 'qwe', 'asd', 'zxc', 'qwe', 'asd', 'zxc']),
+    new Client(10, 'Robert', 'Pavlovich', 'Robert@com.ua', 380101010101, ['qwe', 'asd', 'zxc']),
 ]
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 
 let ClientArrSort = ClientArr.sort((a, b) => b.order > a.order && b.id - a.id)
 console.log('Ascending sorting and order -', ClientArrSort)
-
-
-
 
 // - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску,
 //   максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
@@ -72,6 +69,63 @@ console.log('Ascending sorting and order -', ClientArrSort)
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+function Cars(model, producer, year, maxSpeed, engineCapacity) {
+    return {
+        model,
+        producer,
+        year,
+        maxSpeed,
+        engineCapacity,
+        drive() {
+            console.log('їдемо зі швидкістю - ' + this.maxSpeed + ' - km/h')
+        },
+        info() {
+            console.log('=== info ===')
+            console.log('model - ' + this.model)
+            console.log('producer - ' + this.producer)
+            console.log('year - ' + this.year)
+            console.log('maxSpeed - ' + this.maxSpeed)
+            console.log('engineCapacity - ' + this.engineCapacity)
+
+        },
+        increaseMaxSpeed(newSpeed) {
+            console.log('increaseMaxSpeed - ' + newSpeed)
+            let speed = this.maxSpeed + newSpeed
+            return speed - this.maxSpeed
+        },
+        changeYear(newValue) {
+            console.log('changeYear - ' + newValue)
+            let newYear = this.year + newValue
+            return newYear - this.year
+        },
+        addDriver(driver) {
+            console.log('New driver :')
+            console.log(driver)
+            return {}
+        }
+    }
+}
+
+let car1 = Cars('audi', 'Audi AG', 2010, 300, 3.3)
+
+console.log(car1)
+car1.drive()
+car1.info()
+car1.increaseMaxSpeed(330)
+car1.changeYear(2015)
+car1.addDriver({name: 'Jack', age: 31})
+car1.addDriver({name: 'Rouse', age: 26})
+let car2 = Cars('bmw', 'BMW AG', 2005, 220, 2.5)
+console.log('==================')
+console.log(car2)
+car2.drive()
+car2.info()
+car2.increaseMaxSpeed(250)
+car2.changeYear(2020)
+car2.addDriver({name: 'Tom', age: 20})
+car2.addDriver({name: 'Jerry', age: 15})
+
 
 // - (Те саме, тільки через клас)
 //   Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску,

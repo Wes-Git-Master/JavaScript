12a6572354b1044bf4049c9d01d9d17bf8457223
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-03-12 02:10:28 +0200

    Homework 8 - preparation

--- - Homework/HW-8/main.js
+++ - Homework/HW-8/main.js
@@ -28,19 +28,36 @@ let UserArrFilter = UserArr.filter(value => value.id % 2 - 1)
 console.log('Filtering by even id -', UserArrFilter)
 
 // - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
-let UserArrSort = UserArr.sort((a, b) => b.id - a.id )
+let UserArrSort = UserArr.sort((a, b) => b.id - a.id)
 console.log('Ascending sorting by ID -', UserArrSort)
 
 // - Створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 //   створити пустий масив, наповнити його 10 об'єктами Client
+class Client {
 
+    constructor(id, name, surname, email, phone, order) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.order = order;
+    }
+}
 
-
-
-
-
-
-
+let ClientArr = [
+    new Client(1, 'Max', 'Ivanovich', 'Max@com.ua', 380111111111, ['qwe']),
+    new Client(2, 'Ivan', 'Maximovich', 'Ivan@com.ua', 380222222222,['qwe', 'asd', 'zxc']),
+    new Client(3, 'Yuriy', 'Kirilovich', 'Yuriy@com.ua', 380333333333,['qwe', 'asd']),
+    new Client(4, 'Kiril', 'Yurievich', 'Kiril@com.ua', 380444444444,['qwe', 'asd', 'zxc','qwe', 'asd', 'zxc']),
+    new Client(5, 'Anna', 'Vasylivna', 'Anna@com.ua', 380555555555,['qwe', 'asd', 'zxc','qwe']),
+    new Client(6, 'Olena', 'Stepanivna', 'Olena@com.ua', 380666666666,['qwe', 'asd', 'zxc']),
+    new Client(7, 'Iryna', 'Oleksandrivna', 'Iryna@com.ua', 380777777777,['qwe', 'asd', 'zxc','qwe', 'asd']),
+    new Client(8, 'Stepan', 'Matvyevich', 'Stepan@com.ua', 380888888888,['qwe', 'asd', 'zxc']),
+    new Client(9, 'Anastasia', 'Oleksandrivna', 'Nastya@com.ua', 380999999999,['qwe', 'asd', 'zxc','qwe', 'asd', 'zxc','qwe', 'asd', 'zxc']),
+    new Client(10, 'Robert', 'Pavlovich', 'Robert@com.ua', 380101010101,['qwe', 'asd', 'zxc']),
+]
+console.log(ClientArr);
 
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 

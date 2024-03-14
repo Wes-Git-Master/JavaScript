0bae57dffa60ae79baba53a4ae3eeee60cdefaad
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-03-15 01:42:46 +0200

    Homework 9

--- - Homework/HW-9/main.js
+++ - Homework/HW-9/main.js
@@ -3,32 +3,30 @@
 // - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
 // - додати цей блок в body.
 // - клонувати його повністю, та додати клон в body.
-
+console.log(document.body);
 let div = document.createElement('div')
-div.classList.add('wrap','collapse','alpha','beta');
+div.classList.add('wrap', 'collapse', 'alpha', 'beta');
 div.innerText = 'Text'
-console.log(div)
 document.body.appendChild(div)
 let div2 = div.cloneNode()
 div2.innerText = 'Text 2'
-console.log(div2)
 document.body.appendChild(div2)
 
 // - Є масив:
 //     ['Main','Products','About us','Contacts']
 //   Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
-
-
-
-
-
-
-
-
-
-
-
-
+let arr = ['Main', 'Products', 'About us', 'Contacts'];
+let ul = document.createElement('ul')
+document.body.appendChild(ul)
+let li1 = document.createElement('li')
+let li2 = document.createElement('li')
+let li3 = document.createElement('li')
+let li4 = document.createElement('li')
+li1.innerText = arr[0]
+li2.innerText = arr[1]
+li3.innerHTML = arr[2]
+li4.innerHTML = arr[3]
+ul.append(li1, li2, li3, li4)
 
 // - Є масив
 //   let coursesAndDurationArray = [
@@ -41,6 +39,21 @@ document.body.appendChild(div2)
 //   ];
 //  Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
 //  Завдання робити через цикли.
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (const ArrayElement of coursesAndDurationArray) {
+    let div = document.createElement('div')
+    div.innerText = ` Title - ${ArrayElement.title} | MonthDuration - ${ArrayElement.monthDuration}`
+    document.body.appendChild(div)
+    div.classList.add('courses')
+}
 
 // =========================
 
@@ -56,6 +69,22 @@ document.body.appendChild(div2)
 //   За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,
 //   в якому буде <h1 class='heading'> з title  елементу, та <p class='description'> з monthDuration елементу.
 //   Завдання робити через цикли.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ==========================
 

545ab17bad79f438dbb8f67ee62495b65d5b5a6a
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-03-15 19:50:38 +0200

    Homework 9

--- - Homework/HW-9/main.js
+++ - Homework/HW-9/main.js
@@ -138,15 +138,6 @@ for (const simpson of simpsons) {
     document.body.append(div)
 }
 
-
-
-
-
-
-
-
-
-
 // =========================
 
 //   Цикл в циклі
@@ -155,3 +146,72 @@ for (const simpson of simpsons) {
 //   в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
 //   Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
 
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];

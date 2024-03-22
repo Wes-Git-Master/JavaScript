30ee007b44eca60977adbfa10780a48b29c6bd9c
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-03-22 18:31:42 +0200

    Homework 11

--- - Homework/HW-11/main.js
+++ - Homework/HW-11/main.js
@@ -2,9 +2,10 @@
 // - Взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини.
 //   Відобразити всі поля кожної корзини.
 
-fetch( 'https://dummyjson.com/docs/carts' )
+fetch('https://dummyjson.com/carts')
     .then(res => res.json())
-    .then(value => value.log() )
+    .then(console.log);
+
 // ** task 2
 // - Взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти.
 //   Інгредієнти повинні бути список під час відображення.
